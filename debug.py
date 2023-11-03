3dd2e2057454db54d3f3240e52ada6b6049e7fe2
AbiyuTamirat2 <148640715+AbiyuTamirat2@users.noreply.github.com>
2023-11-02 17:21:56 -0700

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,16 +1,28 @@
 def lone_sum(a, b, c):
 
-    # The line below need to be changed!
-    if a >= b:
-        return c
-    elif a == c:
-        return b
-    elif b == c:
-        return a
-    elif a == b and a == c and b == c:
+# Find the sum of all three numbers, a, b, and c.
+
+    total = a + b + c
+
+# verify if all three values are the same. If the condition is true, then return 0.
+
+    if a == b and a == c and b == c:
         return 0
-    else:
-        return a+b+c
+
+# verify if there are any two values that are the same. If that is the case, return the third value.
+
+    if a == b:
+        return c
+    if a == c:
+        return b
+    if b == c:
+        return a
+
+# If none of those conditions are met, the sum of variables a, b, and c should be returned.
+
+    return total
+
+# Examples for testing purposes.
 
 print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(10, 10, 10)))
 print("lone_sum of 1, 2, 3 should be 6: " + str(lone_sum(1, 2, 3)))
